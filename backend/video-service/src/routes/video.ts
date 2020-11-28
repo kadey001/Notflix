e56d7bc8176b86f6b29c9ee677d7af00ac0b9d72
kadey001 <kadey001@ucr.edu>
2020-11-28 22:52:08 +0000

    Added video view route in video-service Streams file from webhdfs and pipes to response with content range headers.

--- backend/video-service/src/routes/video.ts
+++ backend/video-service/src/routes/video.ts
@@ -1,32 +1,153 @@
 import express from 'express';
+import WebHDFS from 'webhdfs';
+import http from 'http';
+import request from 'request';
 
 import { addFilm, filterGenre, filterviews, filterLikes, filterKeyword } from '../db/postgresql';
 
 const router = express.Router();
+const hdfs = WebHDFS.createClient({
+    user: 'main',
+    host: 'localhost',
+    port: 9870,
+    path: '/webhdfs/v1'
+});
 
-/*router.get('/video/:id', (req, res, next) => {
-    const { id } = req.params; // Get video id from params
-    res.status(200).send();
-});*/
+router.get('/view', (req, res, next) => {
+    if (!req.query.vid) {
+        res.status(400).send('Undefined vid');
+    }
+    const path = `/home/videos/${req.query.vid}/movie.mp4`;
+    // Get stats from video w/ SQL
+    const options = {
+        host: 'localhost',
+        path: `/webhdfs/v1${path}?op=GETFILESTATUS`,
+        port: '9870',
+        method: 'GET'
+    }
+    const statReq = http.request(options, (statRes) => {
+        statRes.on('data', data => {
+            const parsedData = JSON.parse(data);
+            if (!parsedData) {
+                res.status(400).send('Undefined parseData');
+            } else if (!parsedData.FileStatus) {
+                res.status(400).send('Undefined parsedData.FileStatus');
+            }
+            const fileSize = parsedData.FileStatus.length;
+            const range = req.headers.range;
+            if (range) {
+                const parts = range.replace(/bytes=/, "").split("-");
+                const start = parseInt(parts[0], 10);
+                const end = parts[1]
+                    ? parseInt(parts[1], 10)
+                    : fileSize - 1;
+                const chunksize = (end - start) + 1;
+                const stream = hdfs.createReadStream(path, { offset: start, buffersize: 1, length: chunksize });
+                res.on('close', () => {
+                    if (!stream)
+                        return;
+                    stream.req.end();
+                    stream.req.destroy();
+                    stream.end();
+                    stream.destroy();
+                    console.log('Response Closed');
+                });
+                stream.on('error', (err: any) => {
+                    console.error(err);
+                    res.end('Server error');
+                    next(err);
+                });
+                stream.on('close', () => {
+                    console.log('File Closing (206 Response)');
+                });
+                // TODO Get Content-Type from postgres metadata
+                const head = {
+                    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+                    'Accept-Ranges': 'bytes',
+                    'Content-Length': chunksize,
+                    'Content-Type': 'video/mp4',
+                };
+                res.writeHead(206, head);
+                if (!stream)
+                    res.end();
+                else {
+                    stream.pipe(res)
+                        .on('error', (err: any) => {
+                            console.log(err);
+                            next(err);
+                        }).on('finish', () => {
+                            console.log('Pipe Finished');
+                        });
+                    console.log('File Stream Pipe Opened (206 Response)');
+                }
+            } else {
+                const head = {
+                    'Content-Length': fileSize,
+                    'Content-Type': 'video/mp4',
+                };
+                res.writeHead(200, head);
+                const stream = hdfs.createReadStream(path, { buffersize: 1 });
+                res.on('close', () => {
+                    if (!stream)
+                        return;
+                    stream.req.end();
+                    stream.req.destroy();
+                    stream.end();
+                    stream.destroy();
+                    console.log('Response Closed');
+                });
+                stream.on('error', (err: any) => {
+                    console.error(err);
+                    res.end('Server error');
+                    next(err);
+                });
+                // stream.on('close', () => { // pipe & socket additional events
+                //     console.log('File Closing (200 Response)');
+                // });
+                if (stream == null)
+                    res.end();
+                else {
+                    stream.pipe(res)
+                        .on('error', (err: any) => {
+                            console.log(err);
+                            next(err);
+                        }).on('finish', () => {
+                            console.log('Pipe Finished');
+                        });
+                    console.log('File Stream Pipe Opened (200 Response)');
+                }
+            }
+        })
+    });
+    statReq.on('error', (err) => {
+        console.error(err);
+    });
+    statReq.end();
+});
+
+// router.get('/get-previous-view-time', async (req, res, next) => {
+//     if (!req.query.vid) {
+//         res.status(400).send('Undefined vid');
+//         next();
+//         return;
+//     }
+
+//     res.status(200).json({ result: 0.0 })
+// });
 
 router.get('/watch/', (req, res, next) => {
     //const { videoID } = req.query.videoID; // Get video id from params
     const videoID = req.query.videoID;
-    console.log("does this print to our terminal or post "+videoID);
-    console.log("This is our comment ID with console dir "+req.query.commentID);
+    console.log("does this print to our terminal or post " + videoID);
+    console.log("This is our comment ID with console dir " + req.query.commentID);
     //res.status(200).json({result: "The result of id is "+req.params.id}).send();
     res.status(200).send();
 });
 
-/*router.get('/test', (req, res, next) => {
-    console.log('TEST');
-    res.status(200).send();
-});*/
-
 router.post('/test', (req, res, next) => {
-        const { name } = req.body as { name: string };
-        console.log(name);
-        res.status(200).json({result: "The result is "+name}).send();
+    const { name } = req.body as { name: string };
+    console.log(name);
+    res.status(200).json({ result: "The result is " + name }).send();
 });
 
 
@@ -38,8 +159,8 @@ router.post('/newfilm', async (req, res, next) => {
         const { horrorGenre } = req.body as { horrorGenre: boolean };
         const { comedyGenre } = req.body as { comedyGenre: boolean };
         const { actionGenre } = req.body as { actionGenre: boolean };
-        await addFilm(filmLength,filmTitle,comedyGenre,horrorGenre,actionGenre);
-        console.log(filmTitle+", "+filmLength+", "+horrorGenre+", "+comedyGenre+", "+actionGenre);
+        await addFilm(filmLength, filmTitle, comedyGenre, horrorGenre, actionGenre);
+        console.log(filmTitle + ", " + filmLength + ", " + horrorGenre + ", " + comedyGenre + ", " + actionGenre);
         res.status(200).send();
     } catch (err) {
         console.error(err);
@@ -52,11 +173,11 @@ router.get('/filterGenre', async (req, res, next) => {
         let horrorQuery = req.query.horrorGenre;
         let comedyQuery = req.query.comedyGenre;
         let actionQuery = req.query.actionGenre;
-        let horrorBool: boolean = (horrorQuery === 'true');
-        let comedyBool: boolean = (comedyQuery === 'true');
-        let actionBool: boolean = (actionQuery === 'true');
-        await filterGenre(comedyBool,horrorBool,actionBool);
-        console.log(comedyBool+", "+horrorBool+", "+actionBool);
+        let horrorBool = (horrorQuery === 'true');
+        let comedyBool = (comedyQuery === 'true');
+        let actionBool = (actionQuery === 'true');
+        await filterGenre(comedyBool, horrorBool, actionBool);
+        console.log(comedyBool + ", " + horrorBool + ", " + actionBool);
         res.status(200).send();
     } catch (err) {
         console.error(err);
@@ -73,10 +194,10 @@ router.get('/filterViews', async (req, res, next) => {
     try {
         let desiredViewsQuery = req.query.desiredViews;
         let higherOrLowerQuery = req.query.higherOrLower;
-        let desiredViewsInt: number = parseInt(desiredViewsQuery!);
-        let higherOrLowerBool: boolean = (higherOrLowerQuery === 'true');
-        await filterviews(desiredViewsInt,higherOrLowerBool);
-        console.log(desiredViewsInt+", "+higherOrLowerBool);
+        let desiredViewsInt = parseInt(desiredViewsQuery as string);
+        let higherOrLowerBool = (higherOrLowerQuery === 'true');
+        await filterviews(desiredViewsInt, higherOrLowerBool);
+        console.log(desiredViewsInt + ", " + higherOrLowerBool);
         res.status(200).send();
     } catch (err) {
         console.error(err);
@@ -88,10 +209,10 @@ router.get('/filterLikes', async (req, res, next) => {
     try {
         let desiredLikesQuery = req.query.desiredLikes;
         let higherOrLowerQuery = req.query.higherOrLower;
-        let desiredLikesInt: number = parseInt(desiredLikesQuery!);
-        let higherOrLowerBool: boolean = (higherOrLowerQuery === 'true');
-        await filterLikes(desiredLikesInt,higherOrLowerBool);
-        console.log(desiredLikesInt+", "+higherOrLowerBool);
+        let desiredLikesInt = parseInt(desiredLikesQuery as string);
+        let higherOrLowerBool = (higherOrLowerQuery === 'true');
+        await filterLikes(desiredLikesInt, higherOrLowerBool);
+        console.log(desiredLikesInt + ", " + higherOrLowerBool);
         res.status(200).send();
     } catch (err) {
         console.error(err);
@@ -105,11 +226,11 @@ router.get('/search', async (req, res, next) => {
         let horrorQuery = req.query.horrorGenre;
         let comedyQuery = req.query.comedyGenre;
         let actionQuery = req.query.actionGenre;
-        let horrorBool: boolean = (horrorQuery === 'true');
-        let comedyBool: boolean = (comedyQuery === 'true');
-        let actionBool: boolean = (actionQuery === 'true');
-        let FilmString: string = desiredFilmTitle!;
-        await filterKeyword(FilmString,comedyBool,horrorBool,actionBool);
+        let horrorBool = (horrorQuery === 'true');
+        let comedyBool = (comedyQuery === 'true');
+        let actionBool = (actionQuery === 'true');
+        let FilmString = desiredFilmTitle as string;
+        await filterKeyword(FilmString, comedyBool, horrorBool, actionBool);
         console.log();
         res.status(200).send();
     } catch (err) {
