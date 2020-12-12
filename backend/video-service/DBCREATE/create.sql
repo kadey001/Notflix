847096661f6383f3b939b53852e09b0ec2589929
kadey001 <kadey001@ucr.edu>
2020-12-12 00:08:46 +0000

    Updated sql schema

--- backend/video-service/DBCREATE/create.sql
+++ backend/video-service/DBCREATE/create.sql
@@ -1,40 +1,40 @@
-DROP TABLE IF EXISTS User;
-DROP TABLE IF EXISTS Video;
-DROP TABLE IF EXISTS Genres;
-DROP TABLE IF EXISTS Plan;
+DROP TABLE IF EXISTS users CASCADE;
+DROP TABLE IF EXISTS video CASCADE;
+DROP TABLE IF EXISTS genres CASCADE;
+DROP TABLE IF EXISTS plan CASCADE;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Entities
-
-CREATE TABLE Users (
-    uid BIGINT NOT NULL,
+CREATE TABLE users (
+    uid uuid DEFAULT uuid_generate_v4() NOT NULL,
     email VARCHAR(64) NOT NULL,
     username VARCHAR(64) NOT NULL,
     password VARCHAR(64) NOT NULL,
+    token TEXT,
+    plantype INTEGER NOT NULL,
+    created DATE NOT NULL,
     PRIMARY KEY(uid)
 );
 
-CREATE TABLE Video (
-    vid BIGINT NOT NULL,
+CREATE TABLE videos (
+    vid uuid DEFAULT uuid_generate_v4() NOT NULL,
     filmLength BIGINT NOT NULL,
     title VARCHAR(64) NOT NULL,
+    description TEXT NOT NULL,
     likes INTEGER NOT NULL,
     dislikes INTEGER NOT NULL,
     views INTEGER NOT NULL,
+    released DATE NOT NULL,
     PRIMARY KEY(vid)
 );
 
-CREATE TABLE Genres (
-    vid BIGINT NOT NULL,
+CREATE TABLE genres (
+    vid uuid NOT NULL,
     comedy BOOLEAN NOT NULL,
     horror BOOLEAN NOT NULL,
     action BOOLEAN NOT NULL,
+    fantasy BOOLEAN NOT NULL,
+    drama BOOLEAN NOT NULL,
+    documentary BOOLEAN NOT NULL,
     PRIMARY KEY(vid),
-    FOREIGN KEY(vid) REFERENCES Video(vid)
-);
-
-CREATE TABLE Plan (
-    uid BIGINT NOT NULL,
-    planType VARCHAR(64) NOT NULL,
-    PRIMARY KEY(uid),
-    FOREIGN KEY(uid) REFERENCES Users(uid)
+    FOREIGN KEY(vid) REFERENCES video(vid)
 );
